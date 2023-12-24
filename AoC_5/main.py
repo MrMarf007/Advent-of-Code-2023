aaa3ccf6cac43a635ef6fb85ae4c3441bfc4d69c
Stan <s.t.ruiter@students.uu.nl>
2023-12-24 23:35:18 +0100

    little benchmark added

--- AoC_5/main.py
+++ AoC_5/main.py
@@ -13,7 +13,7 @@ class map:
         res = []
         for val in arr:
             num = self.getMapping(val)
-            print (self.name, "mapped: ", val, " -> ", num)
+            #print (self.name, "mapped: ", val, " -> ", num)
             res.append(num)
         return res
     
@@ -38,7 +38,7 @@ class map:
         res = []
         for val in arr:
             num = self.getLowestMappingFromRange(val)
-            print (self.name, "mapped: ", val, " -> ", num)
+            #print (self.name, "mapped: ", val, " -> ", num)
             for n in num:
                 res.append(n)
         return res
@@ -88,13 +88,20 @@ def P2(f):
     (lowest, _) = min(mapSteps)
     return(lowest)
 
+import time
+t1 = time.time()
 
 file = open("input.txt", "r")
 p1 = P1(file)
 print("\n================-----------{ Part 1 result: ", p1, "}-----------================\n")
 
+t2 = time.time()
+
 file = open("input.txt", "r")
 p2 = P2(file)
 
-print("\nPart 1 result: ", p1)
-print("\nPart 2 result: ", p2)
+t3 = time.time()
+print("P1 time: ", (t2 - t1), "\n")
+print("P2 time: ", (t3 - t2), "\n")
+print("Part 1 result: ", p1, "\n")
+print("Part 2 result: ", p2, "\n")
