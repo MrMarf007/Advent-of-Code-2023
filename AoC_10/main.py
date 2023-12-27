74400b7556da2eca71da147c3670745e80e148f1
Stan <s.t.ruiter@students.uu.nl>
2023-12-27 12:36:53 +0100

    d10 fix

--- AoC_10/main.py
+++ AoC_10/main.py
@@ -7,6 +7,8 @@
 # . is ground; there is no pipe in this tile.
 # S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 
+import math
+
 class P1:
     def parse(file):
         grid = []     #2D array representing the grid (y, x)
@@ -14,7 +16,7 @@ class P1:
         for f in file:
             if "S" in f: start = (len(grid), f.index("S"))
             grid.append([*f.strip()])
-        print (grid, start)
+        # print (grid, start)
         return (grid, start)
 
     def run(data):
@@ -25,7 +27,7 @@ class P1:
             paths.append([startPos, m])
         while not P1.collide(paths):
             paths = P1.step(grid, paths)
-        print(paths)
+        # print(paths)
         return(len(paths[0])-1)
     
     def step(grid, paths):
@@ -96,14 +98,78 @@ class P2:
 
     def run(data):
         (grid, startPos) = data
+        (sy,sx) = startPos
         moves = P1.genMoves(grid, startPos, None)
-        paths = []
-        for m in moves:
-            paths.append([startPos, m])
-        while not P1.collide(paths):
-            paths = P1.step(grid, paths)
-        print(paths)
-        return(len(paths[0])-1)
+        
+        # MAKE THIS WORK SO RESULT IS 351
+        isPassable = False
+        (my1, mx1) = moves[0]
+        (my2, mx2) = moves[1]
+        diff = (my1-sy, mx1-sx)
+        (dy1,dx1) = (my1-sy, mx1-sx)
+        (dy2,dx2) = (my2-sy, mx2-sx)
+        diff = (dy1+dy2, dx1+dx2)
+        print(diff)
+        match diff:
+            case (1,1): 
+                isPassable = False
+            case (-1,-1): 
+                isPassable = False
+            case (0,0): 
+                isPassable = False
+            case (-1,1): 
+                isPassable = True
+            case (1,-1): 
+                isPassable = True
+        
+        path = [startPos, moves[0]]
+        while path[-1] != moves[1]:
+            path = P2.step(grid, path)
+        loop = {}
+        i = 0
+        for p in path:
+            loop[p] = grid[p[0]][p[1]]
+        hitsL = []
+        count = 0
+        print("raycasting started!")
+        for y in range(len(grid)):
+            #print (y)
+            for x in range(len(grid[0])):
+                if (y,x) in loop: continue
+                
+                hits = 0
+                dy = y
+                dx = x
+                
+                while dy < len(grid) and dx < len(grid[0]):
+                    if (dy,dx) in loop:
+                        val = loop[(dy,dx)] 
+                        if val != "L" and val != "7":
+                            if not (isPassable and val == "S"):
+                                #print("hit at: ", dy, dx, loop[(dy,dx)])
+                                hits += 1
+                    dy += 1
+                    dx += 1
+                if hits % 2 == 1: 
+                    print("hit at", y, x)
+                    hitsL.append((y,x))
+                    count += 1
+                    
+        for hit in hitsL:
+            loop[hit] = "X"
+                    
+        # for y in range(len(grid)):
+        #     print ()
+        #     for x in range(len(grid[0])):
+        #         if (y,x) in loop: print(loop[(y,x)], end="") 
+        #         else: print("O", end="")
+                
+        return(count)
+    
+    
+    def step(grid, path):
+        newMove = P1.genMoves(grid, path[-1], path[-2])
+        return path + newMove
         
 
 
